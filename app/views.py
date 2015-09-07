5825d11a095bd9d2bff1f9598ce9291c3e95551d
Ernesto Vargas <me@netoxico.com>
2015-09-07 11:37:47 -0500

    first commit :D

--- app/views.py
+++ app/views.py
@@ -8,12 +8,12 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
-    'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
-    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+    'name' : 'Ernesto Vargas',
+    'bio' : 'Software Developer visitin from Mexico City :D',
+    'email' : 'me@netoxico.com', # Leave blank if you'd prefer not to share your email with other conference attendees
+    'twitter_username' : 'netoxico', # No @ symbol, just the handle.
+    'github_username' : "netoxico", 
+    'headshot_url' : 'http://netoxico.com', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
 def home(request):
